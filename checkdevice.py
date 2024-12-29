54513ced92059b94aeeaa03e29ac763987aee877
Erwanweb <44497874+Erwanweb@users.noreply.github.com>
2024-12-29 11:36:51 +0100

    Delete checkdevice.py

--- checkdevice.py
+++
@@ -1,7 +0,0 @@
-# Checking variable of tuya device
-import tinytuya
-import tuyadevice
-
-d = tinytuya.Device(tuyadevice.DEVICE_ID, tuyadevice.IP_ADDRESS, tuyadevice.LOCAL_KEY, version=tuyadevice.VERSION)
-data = d.status() 
-print('Device status: %r' % data)
